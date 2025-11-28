91e738335d08ab20c5196a3073f511e18d756553
root <root@RAEW.raew>
2025-11-28 11:28:01 +0545

    upload BLN Project

---
+++ script.js
@@ -0,0 +1,6 @@
+// Mobile menu toggle
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('mobile-menu');
+btn.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
